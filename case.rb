467b8ad6878e88ed06850b6d0445b578ba2f0340
Nat <natashaslater01@gmail.com>
2016-01-05 15:19:02 -0500

    Update case.rb

--- case.rb
+++ case.rb
@@ -5,7 +5,7 @@ puts "Better bring an umbrella!"
 when 'sunny'
 puts "Leave that jacket at home"
 when 'foggy'
-puts "Make sur to run your headlights!"
+puts "Make sure to run your headlights!"
 when 'cloudy'
 puts "You're not going to need your sunglasses today!"
 end
